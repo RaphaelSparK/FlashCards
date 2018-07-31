04d844dac5236a46190bfc4cfc1d4e70812d60db
RaphaelSparK <raphael.spark@gmail.com>
2018-07-30 22:27:16 -0300

    Adicionei o primeiro deck

---
+++ flashcards/Components/Decks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text } from 'react-native'
+
+class Decks extends Component {
+  render () {
+    return (
+      <Text>Teste</Text>
+    )
+  }
+}
+
+export default Decks
